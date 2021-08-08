8fe4837867ae82ba5ca50a6494eb5d7d99c5e2bd
Bart de Bever <bartdebever112@gmail.com>
2021-08-08 16:22:21 +0200

    Restructure lib Add WPG and BrawlLib Upgrade project to .NET 5

---
+++ MasterHand/MasterHand/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace MasterHand
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
